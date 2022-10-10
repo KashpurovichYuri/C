f1746cd8a6805a509188673bd2c26f644e773028
Yuri Kashpurovich <101568465+KashpurovichYuri@users.noreply.github.com>
2022-10-10 13:25:35 +0300

    Add files via upload

--- homework05/Class_01/Lands.h
+++ homework05/Class_01/Lands.h
@@ -9,67 +9,67 @@ class Lands
 	using square_km2 = int;
 	using thousands_of_people = int;
 
-	public:
+public:
 
-		Lands() = default;
+	Lands() = default;
 
-		Lands(int area
-			, int population
-			, const std::string& national_currency
-			, const std::string& official_language
-			, const std::string& capital
-			, const std::string& country)
-			: area { area }
-			, population { population }
-			, national_currency { national_currency }
-			, official_language { official_language }
-			, capital { capital }
-			, country { country }
-		{
+	Lands(int area
+		, int population
+		, const std::string& national_currency
+		, const std::string& official_language
+		, const std::string& capital
+		, const std::string& country)
+		: area { area }
+		, population { population }
+		, national_currency { national_currency }
+		, official_language { official_language }
+		, capital { capital }
+		, country { country }
+	{
 
-		}
+	}
 
-		~Lands() = default;
+	~Lands() = default;
 
-		void printCountry() const
-		{
-			std::cout << "Country: " << country << std::endl;
-		}
+	void printCountry() const
+	{
+		std::cout << "Country: " << country << std::endl;
+	}
 
-		void printCapital() const
-		{
-			std::cout << "Capital: " << capital << std::endl;
-		}
+	void printCapital() const
+	{
+		std::cout << "Capital: " << capital << std::endl;
+	}
 
-		void printOfLanguage() const
-		{
-			std::cout << "Official language: " << official_language << std::endl;
-		}
+	void printOfLanguage() const
+	{
+		std::cout << "Official language: " << official_language << std::endl;
+	}
 
-		void printNatCurrency() const
-		{
-			std::cout << "National currency: " << national_currency << std::endl;
-		}
+	void printNatCurrency() const
+	{
+		std::cout << "National currency: " << national_currency << std::endl;
+	}
 
-		void printArea() const
-		{
-			std::cout << "Area, km2: " << area << std::endl;
-		}
+	void printArea() const
+	{
+		std::cout << "Area, km2: " << area << std::endl;
+	}
 
-		void printPopulation() const
-		{
-			std::cout << "Population, thousands of people: " << population << std::endl;
-		}
+	void printPopulation() const
+	{
+		std::cout << "Population, thousands of people: " << population << std::endl;
+	}
 
-		void printPopDensity() const;
-		void printInform() const;
+	void printPopDensity() const;
+	void printInform() const;
 
-	private:
-		static constexpr auto to_neccesary_unit { 1000 };
-		square_km2 area;
-		thousands_of_people population;
-		std::string national_currency;
-		std::string official_language;
-		std::string capital;
-		std::string country;
+private:
+	static constexpr auto to_neccesary_unit { 1000 };
+	square_km2 area;
+	thousands_of_people population;
+	std::string national_currency;
+	std::string official_language;
+	std::string capital;
+	std::string country;
 };
