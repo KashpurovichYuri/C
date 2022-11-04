83dd66dd293def1c2d93da44757733bc0b25cc43
Yuri Kashpurovich <101568465+KashpurovichYuri@users.noreply.github.com>
2022-11-04 15:18:55 +0300

    Add files via upload

--- homework08/Paths04.cpp
+++ homework08/Paths04.cpp
@@ -22,10 +22,13 @@ String evaluate_salary_and_return_name(Employee e)
 /*
 	Execution paths with exceptions:
 	+3 if e.title(), or e.salary(), or they both generate exceptions (if value of the logic expression if fully estimated)
+	+3 if operator==, or operator>, or they both in logic expression generate exceptions
 	+3 if e.name() or e.surname() or they both generate exceptions in if-branch of code
+	+3 if operator<< generate exceptions (with e.name(), e.surname() or them both) in if-branch of code
 	+1 if e.name() generates exceptions in else-branch of code
-	+0 if e.name() generates exceptions in if-branch of code or else-branch of code or does not generate it
-	+0 if e.surname() generates exceptions earlier or not
+	+1 if operator<< generates exceptions in else-branch of code
+	+0 due to return if e.name() generates exceptions in if-branch of code or else-branch of code or does not generate it
+	+0 due to return if e.surname() generates exceptions earlier or not
 */
 
-//  Number of execution paths: 9.
+//  Number of execution paths: 16.
